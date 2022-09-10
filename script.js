2a8f14031da49fb709069b514ddf092e49de227b
Sanjarbek Urolov <107649644+sanjarbekurolov1121@users.noreply.github.com>
2022-09-10 06:12:48 -0700

    dfbjbfhhjbhjf

---
+++ script.js
@@ -0,0 +1,42 @@
+
+  const card = document.querySelector(".card");
+  let surat=`https://pbs.twimg.com/profile_images/1055413669612478468/Sk-XpJaW_400x400.jpg`
+
+
+
+  // function addCard(){
+//     return (
+//         card.innerHTML+=`<div class="card-div">
+//         <div class="card-images">
+//         <img src="${surat}">
+//         <div class="card-content">
+//           <h1>New Surface Pro 7</h1>
+//           <p>lorem dkjsfj ekjrfhe jdfksj hbfwjbfewj jbfwkjbfwk</p>
+//           <h3>LEARN MORE</h3>
+//      </div>
+//      </div>
+//       </div>`
+//     ) 
+          
+// }
+
+let btn=document.getElementById('btn');
+
+const myFunction=()=>{
+    btn.addEventListener('mouseover' , ()=>{
+        return (
+                    card.innerHTML+=`<div class="card-div">
+                    <div class="card-images">
+                    <img src="${surat}">
+                    <div class="card-content">
+                      <h1>New Surface Pro 7</h1>
+                      <p>lorem dkjsfj ekjrfhe jdfksj hbfwjbfewj jbfwkjbfwk</p>
+                      <h3>LEARN MORE</h3>
+                 </div>
+                 </div>
+                  </div>`
+                ) 
+    })
+}
+
+myFunction();
