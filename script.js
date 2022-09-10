00104836d5d54280adac3951ac53d504306d68c1
Sanjarbek Urolov <107649644+sanjarbekurolov1121@users.noreply.github.com>
2022-09-10 11:50:15 -0700

    1111

--- script.js
+++ script.js
@@ -4,39 +4,39 @@
 
 
 
-  // function addCard(){
-//     return (
-//         card.innerHTML+=`<div class="card-div">
-//         <div class="card-images">
-//         <img src="${surat}">
-//         <div class="card-content">
-//           <h1>New Surface Pro 7</h1>
-//           <p>lorem dkjsfj ekjrfhe jdfksj hbfwjbfewj jbfwkjbfwk</p>
-//           <h3>LEARN MORE</h3>
-//      </div>
-//      </div>
-//       </div>`
-//     ) 
+  function addCard(){
+    return (
+        card.innerHTML+=`<div class="card-div">
+        <div class="card-images">
+        <img src="${surat}">
+        <div class="card-content">
+          <h1>New Surface Pro 7</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          <h3>LEARN MORE</h3>
+     </div>
+     </div>
+      </div>`
+    ) 
           
-// }
-
-let btn=document.getElementById('btn');
-
-const myFunction=()=>{
-    btn.addEventListener('mouseover' , ()=>{
-        return (
-                    card.innerHTML+=`<div class="card-div">
-                    <div class="card-images">
-                    <img src="${surat}">
-                    <div class="card-content">
-                      <h1>New Surface Pro 7</h1>
-                      <p>lorem dkjsfj ekjrfhe jdfksj hbfwjbfewj jbfwkjbfwk</p>
-                      <h3>LEARN MORE</h3>
-                 </div>
-                 </div>
-                  </div>`
-                ) 
-    })
 }
 
-myFunction();
+// let btn=document.getElementById('btn');
+
+// const myFunction=()=>{
+//     btn.addEventListener('mouseover' , ()=>{
+//         return (
+//                     card.innerHTML+=`<div class="card-div">
+//                     <div class="card-images">
+//                     <img src="${surat}">
+//                     <div class="card-content">
+//                       <h1>New Surface Pro 7</h1>
+//                       <p>lorem dkjsfj ekjrfhe jdfksj hbfwjbfewj jbfwkjbfwk</p>
+//                       <h3>LEARN MORE</h3>
+//                  </div>
+//                  </div>
+//                   </div>`
+//                 ) 
+//     })
+// }
+
+// myFunction();
